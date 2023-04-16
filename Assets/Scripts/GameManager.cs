0449e45907a33fe01d0c8e61ed95513a66f70f99
tolelom <tolelom@naver.com>
2023-04-17 06:01:52 +0900

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Game");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStartButton() {
+        SceneManager.LoadScene("IntroScene");
+    }
+}
