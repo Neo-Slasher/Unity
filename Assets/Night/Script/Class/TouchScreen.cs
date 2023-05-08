422d7ad41af8e38a8d70ad2c6bef669d6fe67ea5
CeronMoa <ceron999@naver.com>
2023-05-08 19:33:16 +0900

    230508/정윤석/조이스틱 삭제 + 적 맵 외부에서 출현하도록 함수 수정
    230508
    1. 조이스틱 삭제 -> 아무 곳이나 누르고 드래그한 방향으로 이동
    2. 적 오브젝트 맵 외부에서 생성

---
+++ Assets/Night/Script/Class/TouchScreen.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    NightManager nightManager;
+
+    [SerializeField]
+    Character character;
+
+    Vector3 startPos;
+    Vector3 moveVector;
+
+    void Start()
+    {
+        moveVector = Vector3.zero;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (!nightManager.isStageEnd)
+        {
+            startPos = eventData.position;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!nightManager.isStageEnd)
+        {
+            Vector3 currVector = eventData.position;
+
+            moveVector = (currVector - startPos).normalized;
+
+            
+            character.CharacterMove(moveVector);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!nightManager.isStageEnd)
+        {
+            character.CharacterStop(Vector3.zero);
+            startPos = Vector3.zero;
+            moveVector = Vector3.zero;
+        }
+    }
+}
