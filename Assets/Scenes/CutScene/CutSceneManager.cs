b257208f63ce86c3d2d1242d128b4654057b517b
tolelom <98kimsungmin@naver.com>
2023-07-13 00:35:52 +0900

    0713

--- Assets/Scenes/CutScene/CutSceneManager.cs
+++ Assets/Scenes/CutScene/CutSceneManager.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CutSceneManager : MonoBehaviour
 {
+    public TextMeshProUGUI typingText;
+    public string message;
+    public float speed = 0.07f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        message = "당신은 돈을 모아 성공적으로 인공 신체 시술을 받을 수 있었습니다. 그러나 당신은 지금까지 '화타'가 불안정한 인공 신체를 고의로 제공하고 있었다는 사실을 밝혀내었습니다.";
+        StartCoroutine(Typing(typingText, message, speed));
     }
 
     // Update is called once per frame
@@ -30,6 +37,15 @@ public class CutSceneManager : MonoBehaviour
         Debug.Log(DataManager.instance.equipmentList.equipment[0].index);
         Debug.Log(DataManager.instance.assassinationStageList.assassinationStage[0].stageNo);
 
+    }
+
+    IEnumerator Typing(TextMeshProUGUI typingText, string message, float speed)
+    {
+        for (int i = 0; i < message.Length; i++)
+        {
+            typingText.text = message.Substring(0, i + 1);
+            yield return new WaitForSeconds(speed);
+        }
     }
 }
 
