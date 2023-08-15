840dfbe9a6fb5dc3fd098de13e76934bbb207782
tolelom <98kimsungmin@naver.com>
2023-08-16 06:24:58 +0900

    0816

--- Assets/Scenes/Prepare/PreparationManager.cs
+++ Assets/Scenes/Prepare/PreparationManager.cs
@@ -11,17 +11,22 @@ using static PreparationManager;
 
 public class PreparationManager : MonoBehaviour
 {
-    TraitManager traitManager;
+    public PreparationTraitManager traitManager;
 
     public TextMeshProUGUI levelText;
     public TextMeshProUGUI expText;
     public TextMeshProUGUI maxHpText;
+    public TextMeshProUGUI addMaxHpText;
     public TextMeshProUGUI moveSpeedText;
+    public TextMeshProUGUI addMoveSpeedText;
     public TextMeshProUGUI attackPowerText;
+    public TextMeshProUGUI addAttackPowerText;
     public TextMeshProUGUI attackSpeedText;
+    public TextMeshProUGUI addAttackSpeedText;
     public TextMeshProUGUI attackRangeText;
+    public TextMeshProUGUI addAttackRangeText;
     public TextMeshProUGUI moneyText;
-    public TextMeshProUGUI getMoneyText;
+    public TextMeshProUGUI getMoneyText;    
 
     public Button difficultyLeftButton;
     public Button difficultyRightButton;
@@ -63,7 +68,7 @@ public class PreparationManager : MonoBehaviour
         ActivateDifficultyButton();
         LoadTraitButton();
         LoadSelectedTraitUI();
-        //UnActiveTraitButton();
+        UnActiveTraitButton();
         UnActiveTraitBoard();
     }
 
@@ -163,7 +168,9 @@ public class PreparationManager : MonoBehaviour
         enemyRespawnText.text = "- 적 개체수 +" + DataManager.instance.difficultyList.difficulty[GameManager.instance.gameDifficulty].enemyRespawn.ToString() + "%";
     }
 
+
     public void OnClickStartButton() {
+        traitManager.SaveTrait();
         SceneManager.LoadScene("DayScene");
     }
 
@@ -171,6 +178,16 @@ public class PreparationManager : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+
+    public void LoadAddStatus() {
+        addMaxHpText.text = "+" + traitManager.dummyPlayer.maxHp;
+        addMoveSpeedText.text = "+" + traitManager.dummyPlayer.moveSpeed;
+        addAttackPowerText.text = "+" + traitManager.dummyPlayer.attackPower;
+        addAttackSpeedText.text = "+" + traitManager.dummyPlayer.attackSpeed;
+        addAttackRangeText.text = "+" + traitManager.dummyPlayer.attackRange;
+        getMoneyText.text = (GameManager.instance.player.earnMoney + traitManager.dummyPlayer.earnMoney).ToString();
+    }
+
 
 
     public void OnClickFirstTraitButton() {
@@ -205,230 +222,238 @@ public class PreparationManager : MonoBehaviour
     public void OnClickTraitButton() {
         GameObject button = EventSystem.current.currentSelectedGameObject;
         int traitNumber = int.Parse(button.name);
+        Trait tempTrait = DataManager.instance.traitList.trait[traitNumber - 1];
 
         DisableTraitInSameLevel(traitNumber);
-
+        
+        // activae trait
+        traitManager.activeTrait(tempTrait);
         GameManager.instance.player.trait[traitNumber] = true;
+        button.GetComponent<Button>().interactable = false;
 
-        Button traitButton = button.GetComponent<Button>();
-        traitButton.interactable = false;
-
-        Trait tempTrait = DataManager.instance.traitList.trait[traitNumber - 1];
         traitName.text = tempTrait.name;
         traitLv.text = tempTrait.requireLv.ToString() + " / " + (tempTrait.rank == 0 ? "일반" : "핵심");
         traitScript.text = tempTrait.script;
         traitImage.sprite = button.GetComponent<Image>().sprite;
         traitImage.transform.GetChild(0).gameObject.GetComponent<Image>().sprite = button.transform.GetChild(1).gameObject.GetComponent<Image>().sprite;
 
+        LoadAddStatus();
+    }
+
+
+    void DisableTrait(int traitNumber) {
+        traitManager.unactiveTrait(DataManager.instance.traitList.trait[traitNumber - 1]);
+        traitButtons[traitNumber].interactable = true;
+        GameManager.instance.player.trait[traitNumber] = false;
     }
 
     void DisableTraitInSameLevel(int traitNumber) {
         switch (traitNumber) {
             case 1:
             case 2:
-                traitButtons[1].interactable = true;
-                traitButtons[2].interactable = true;
-                GameManager.instance.player.trait[1] = false;
-                GameManager.instance.player.trait[2] = false;
-                break;
+                if (GameManager.instance.player.trait[1])
+                    DisableTrait(1);
+                if (GameManager.instance.player.trait[2])
+                    DisableTrait(2);
+                break; 
             case 3:
             case 4:
             case 5:
-                traitButtons[3].interactable = true;
-                traitButtons[4].interactable = true;
-                traitButtons[5].interactable = true;
-                GameManager.instance.player.trait[3] = false;
-                GameManager.instance.player.trait[4] = false;
-                GameManager.instance.player.trait[5] = false;
+                if (GameManager.instance.player.trait[3])
+                    DisableTrait(3);
+                if (GameManager.instance.player.trait[4])
+                    DisableTrait(4);
+                if (GameManager.instance.player.trait[5])
+                    DisableTrait(5);
                 break;
             case 6:
             case 7:
             case 8:
-                traitButtons[6].interactable = true;
-                traitButtons[7].interactable = true;
-                traitButtons[8].interactable = true;
-                GameManager.instance.player.trait[6] = false;
-                GameManager.instance.player.trait[7] = false;
-                GameManager.instance.player.trait[8] = false;
+                if (GameManager.instance.player.trait[6])
+                    DisableTrait(6);
+                if (GameManager.instance.player.trait[7])
+                    DisableTrait(7);
+                if (GameManager.instance.player.trait[8])
+                    DisableTrait(8);
                 break;
             case 9:
             case 10:
-                traitButtons[9].interactable = true;
-                traitButtons[10].interactable = true;
-                GameManager.instance.player.trait[9] = false;
-                GameManager.instance.player.trait[10] = false;
+                if (GameManager.instance.player.trait[9])
+                    DisableTrait(9);
+                if (GameManager.instance.player.trait[10])
+                    DisableTrait(10);
                 break;
             case 11:
             case 12:
             case 13:
-                traitButtons[11].interactable = true;
-                traitButtons[12].interactable = true;
-                traitButtons[13].interactable = true;
-                GameManager.instance.player.trait[11] = false;
-                GameManager.instance.player.trait[12] = false;
-                GameManager.instance.player.trait[13] = false;
+                if (GameManager.instance.player.trait[11])
+                    DisableTrait(11);
+                if (GameManager.instance.player.trait[12])
+                    DisableTrait(12);
+                if (GameManager.instance.player.trait[13])
+                    DisableTrait(13);
                 break;
             case 14:
             case 15:
-                traitButtons[14].interactable = true;
-                traitButtons[15].interactable = true;
-                GameManager.instance.player.trait[14] = false;
-                GameManager.instance.player.trait[15] = false;
+                if (GameManager.instance.player.trait[14])
+                    DisableTrait(14);
+                if (GameManager.instance.player.trait[15])
+                    DisableTrait(15);
                 break;
             case 16:
             case 17:
             case 18:
             case 19:
-                traitButtons[16].interactable = true;
-                traitButtons[17].interactable = true;
-                traitButtons[18].interactable = true;
-                traitButtons[19].interactable = true;
-                GameManager.instance.player.trait[16] = false;
-                GameManager.instance.player.trait[17] = false;
-                GameManager.instance.player.trait[18] = false;
-                GameManager.instance.player.trait[19] = false;
+                if (GameManager.instance.player.trait[16])
+                    DisableTrait(16);
+                if (GameManager.instance.player.trait[17])
+                    DisableTrait(17);
+                if (GameManager.instance.player.trait[18])
+                    DisableTrait(18);
+                if (GameManager.instance.player.trait[19])
+                    DisableTrait(19);
                 break;
             case 20:
             case 21:
             case 22:
-                traitButtons[20].interactable = true;
-                traitButtons[21].interactable = true;
-                traitButtons[22].interactable = true;
-                GameManager.instance.player.trait[20] = false;
-                GameManager.instance.player.trait[21] = false;
-                GameManager.instance.player.trait[22] = false;
+                if (GameManager.instance.player.trait[20])
+                    DisableTrait(20);
+                if (GameManager.instance.player.trait[21])
+                    DisableTrait(21);
+                if (GameManager.instance.player.trait[22])
+                    DisableTrait(22);
                 break;
             case 23:
             case 24:
             case 25:
-                traitButtons[23].interactable = true;
-                traitButtons[24].interactable = true;
-                traitButtons[25].interactable = true;
-                GameManager.instance.player.trait[23] = false;
-                GameManager.instance.player.trait[24] = false;
-                GameManager.instance.player.trait[25] = false;
+                if (GameManager.instance.player.trait[23])
+                    DisableTrait(23);
+                if (GameManager.instance.player.trait[24])
+                    DisableTrait(24);
+                if (GameManager.instance.player.trait[25])
+                    DisableTrait(25);
                 break;
             case 26:
             case 27:
             case 28:
             case 29:
-                traitButtons[26].interactable = true;
-                traitButtons[27].interactable = true;
-                traitButtons[28].interactable = true;
-                traitButtons[29].interactable = true;
-                GameManager.instance.player.trait[26] = false;
-                GameManager.instance.player.trait[27] = false;
-                GameManager.instance.player.trait[28] = false;
-                GameManager.instance.player.trait[29] = false;
+                if (GameManager.instance.player.trait[26])
+                    DisableTrait(26);
+                if (GameManager.instance.player.trait[27])
+                    DisableTrait(27);
+                if (GameManager.instance.player.trait[28])
+                    DisableTrait(28);
+                if (GameManager.instance.player.trait[29])
+                    DisableTrait(29);
                 break;
             case 30:
             case 31:
             case 32:
-                traitButtons[30].interactable = true;
-                traitButtons[31].interactable = true;
-                traitButtons[32].interactable = true;
-                GameManager.instance.player.trait[30] = false;
-                GameManager.instance.player.trait[31] = false;
-                GameManager.instance.player.trait[32] = false;
+                if (GameManager.instance.player.trait[30])
+                    DisableTrait(30);
+                if (GameManager.instance.player.trait[31])
+                    DisableTrait(31);
+                if (GameManager.instance.player.trait[32])
+                    DisableTrait(32);
                 break;
             case 33:
             case 34:
             case 35:
-                traitButtons[33].interactable = true;
-                traitButtons[34].interactable = true;
-                traitButtons[35].interactable = true;
-                GameManager.instance.player.trait[33] = false;
-                GameManager.instance.player.trait[34] = false;
-                GameManager.instance.player.trait[35] = false;
+                if (GameManager.instance.player.trait[33])
+                    DisableTrait(33);
+                if (GameManager.instance.player.trait[34])
+                    DisableTrait(34);
+                if (GameManager.instance.player.trait[35])
+                    DisableTrait(35);
                 break;
             case 36:
             case 37:
             case 38:
-                traitButtons[36].interactable = true;
-                traitButtons[37].interactable = true;
-                traitButtons[38].interactable = true;
-                GameManager.instance.player.trait[36] = false;
-                GameManager.instance.player.trait[37] = false;
-                GameManager.instance.player.trait[38] = false;
+                if (GameManager.instance.player.trait[36])
+                    DisableTrait(36);
+                if (GameManager.instance.player.trait[37])
+                    DisableTrait(37);
+                if (GameManager.instance.player.trait[38])
+                    DisableTrait(38);
                 break;
             case 39:
             case 40:
             case 41:
-                traitButtons[39].interactable = true;
-                traitButtons[40].interactable = true;
-                traitButtons[41].interactable = true;
-                GameManager.instance.player.trait[39] = false;
-                GameManager.instance.player.trait[40] = false;
-                GameManager.instance.player.trait[41] = false;
+                if (GameManager.instance.player.trait[39])
+                    DisableTrait(39);
+                if (GameManager.instance.player.trait[40])
+                    DisableTrait(40);
+                if (GameManager.instance.player.trait[41])
+                    DisableTrait(41);
                 break;
             case 42:
             case 43:
             case 44:
             case 45:
-                traitButtons[42].interactable = true;
-                traitButtons[43].interactable = true;
-                traitButtons[44].interactable = true;
-                traitButtons[45].interactable = true;
-                GameManager.instance.player.trait[42] = false;
-                GameManager.instance.player.trait[43] = false;
-                GameManager.instance.player.trait[44] = false;
-                GameManager.instance.player.trait[45] = false;
+                if (GameManager.instance.player.trait[42])
+                    DisableTrait(42);
+                if (GameManager.instance.player.trait[43])
+                    DisableTrait(43);
+                if (GameManager.instance.player.trait[44])
+                    DisableTrait(44);
+                if (GameManager.instance.player.trait[45])
+                    DisableTrait(45);
                 break;
             case 46:
             case 47:
             case 48:
-                traitButtons[46].interactable = true;
-                traitButtons[47].interactable = true;
-                traitButtons[48].interactable = true;
-                GameManager.instance.player.trait[46] = false;
-                GameManager.instance.player.trait[47] = false;
-                GameManager.instance.player.trait[48] = false;
+                if (GameManager.instance.player.trait[46])
+                    DisableTrait(46);
+                if (GameManager.instance.player.trait[47])
+                    DisableTrait(47);
+                if (GameManager.instance.player.trait[48])
+                    DisableTrait(48);
                 break;
             case 49:
             case 50:
             case 51:
             case 52:
-                traitButtons[49].interactable = true;
-                traitButtons[50].interactable = true;
-                traitButtons[51].interactable = true;
-                traitButtons[52].interactable = true;
-                GameManager.instance.player.trait[49] = false;
-                GameManager.instance.player.trait[50] = false;
-                GameManager.instance.player.trait[51] = false;
-                GameManager.instance.player.trait[52] = false;
+                if (GameManager.instance.player.trait[49])
+                    DisableTrait(49);
+                if (GameManager.instance.player.trait[50])
+                    DisableTrait(50);
+                if (GameManager.instance.player.trait[51])
+                    DisableTrait(51);
+                if (GameManager.instance.player.trait[52])
+                    DisableTrait(52);
                 break;
             case 53:
             case 54:
             case 55:
-                traitButtons[53].interactable = true;
-                traitButtons[54].interactable = true;
-                traitButtons[55].interactable = true;
-                GameManager.instance.player.trait[53] = false;
-                GameManager.instance.player.trait[54] = false;
-                GameManager.instance.player.trait[55] = false;
+                if (GameManager.instance.player.trait[53])
+                    DisableTrait(53);
+                if (GameManager.instance.player.trait[54])
+                    DisableTrait(54);
+                if (GameManager.instance.player.trait[55])
+                    DisableTrait(55);
                 break;
             case 56:
             case 57:
             case 58:
-                traitButtons[56].interactable = true;
-                traitButtons[57].interactable = true;
-                traitButtons[58].interactable = true;
-                GameManager.instance.player.trait[56] = false;
-                GameManager.instance.player.trait[57] = false;
-                GameManager.instance.player.trait[58] = false;
+                if (GameManager.instance.player.trait[56])
+                    DisableTrait(56);
+                if (GameManager.instance.player.trait[57])
+                    DisableTrait(57);
+                if (GameManager.instance.player.trait[58])
+                    DisableTrait(58);
                 break;
             case 59:
             case 60:
             case 61:
             case 62:
-                traitButtons[59].interactable = true;
-                traitButtons[60].interactable = true;
-                traitButtons[61].interactable = true;
-                traitButtons[62].interactable = true;
-                GameManager.instance.player.trait[59] = false;
-                GameManager.instance.player.trait[60] = false;
-                GameManager.instance.player.trait[61] = false;
-                GameManager.instance.player.trait[62] = false;
+                if (GameManager.instance.player.trait[59])
+                    DisableTrait(59);
+                if (GameManager.instance.player.trait[60])
+                    DisableTrait(60);
+                if (GameManager.instance.player.trait[61])
+                    DisableTrait(61);
+                if (GameManager.instance.player.trait[62])
+                    DisableTrait(62);
                 break;
 
         }
--- Assets/Scenes/Prepare/PreparationTraitManager.cs
+++ Assets/Scenes/Prepare/PreparationTraitManager.cs
@@ -83,4 +83,43 @@ public class PreparationTraitManager : MonoBehaviour
         }
     }
 
+
+    public void unactiveTrait(Trait trait) {
+        TraitParseAndDisapply(trait);
+    }
+
+    public void TraitParseAndDisapply(Trait trait) {
+        if (trait.effectType1 != 0)
+            disapplyTrait((EffectType)trait.effectType1, trait.effectValue1, trait.effectMulti1);
+        if (trait.effectType2 != 0)
+            disapplyTrait((EffectType)trait.effectType2, trait.effectValue2, trait.effectMulti2);
+        if (trait.effectType3 != 0)
+            disapplyTrait((EffectType)trait.effectType3, trait.effectValue3, trait.effectMulti3);
+        if (trait.effectType4 != 0)
+            disapplyTrait((EffectType)trait.effectType4, trait.effectValue4, trait.effectMulti4);
+    }
+
+    private void disapplyTrait(EffectType type, double value, double multi) {
+        applyTrait(type, -value, multi); //TODO: multi는 사용처를 몰라서 확인해 볼 필요성이 있음 
+    }
+
+    public void SaveTrait() {
+        GameManager.instance.player.maxHp += dummyPlayer.maxHp;
+        GameManager.instance.player.moveSpeed += dummyPlayer.moveSpeed;
+        GameManager.instance.player.attackPower += dummyPlayer.attackPower;
+        GameManager.instance.player.attackSpeed += dummyPlayer.attackSpeed;
+        GameManager.instance.player.attackRange += dummyPlayer.attackRange;
+        GameManager.instance.player.startMoney += dummyPlayer.startMoney;
+        GameManager.instance.player.earnMoney += dummyPlayer.earnMoney;
+        GameManager.instance.player.shopSlot += dummyPlayer.shopSlot;
+        GameManager.instance.player.itemSlot += dummyPlayer.itemSlot;
+        GameManager.instance.player.shopMinRank += dummyPlayer.shopMinRank;
+        GameManager.instance.player.shopMaxRank += dummyPlayer.shopMaxRank;
+        GameManager.instance.player.dropRank += dummyPlayer.dropRank;
+        GameManager.instance.player.dropRate += dummyPlayer.dropRate;
+        GameManager.instance.player.healByHit += dummyPlayer.healByHit;
+        GameManager.instance.player.hpRegen += dummyPlayer.hpRegen;
+        GameManager.instance.player.dealOnMaxHp += dummyPlayer.dealOnMaxHp;
+        GameManager.instance.player.dealOnCurHp += dummyPlayer.dealOnCurHp;
+    }
 }
