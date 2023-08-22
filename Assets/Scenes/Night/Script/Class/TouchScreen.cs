54abf8d5360107fcab4aa4ca52ebe9e5a966345b
CeronMoa <56114106+ceron999@users.noreply.github.com>
2023-08-22 23:25:20 +0900

    230822/정윤석/조이스틱 끝
    조이스틱 끝

--- Assets/Scenes/Night/Script/Class/TouchScreen.cs
+++ Assets/Scenes/Night/Script/Class/TouchScreen.cs
@@ -30,7 +30,6 @@ public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log(1);
         if (!nightManager.isStageEnd)
         {
             startPos = eventData.position;
@@ -46,19 +45,12 @@ public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
         if (!nightManager.isStageEnd)
         {
-            Debug.Log(2);
             Vector3 currVector = eventData.position;
 
             Vector3 touchPos = eventData.position - (Vector2)startPos;
 
-            Vector3 joystickPos = joyStickRecTransform.anchoredPosition;
-            Vector3 currPos = touchPos - joystickPos;
-
-            Vector3 clampedPos = currPos.magnitude < joyStickRange ?
-                currPos : currPos.normalized * joyStickRange;
-            if (currPos.magnitude < joyStickRange)
-                Debug.Log("in");
-            else Debug.Log("Out");
+            Vector3 clampedPos = touchPos.magnitude < joyStickRange ?
+                touchPos : touchPos.normalized * joyStickRange;
 
 
             joyStickRecTransform.anchoredPosition = clampedPos;
@@ -71,7 +63,6 @@ public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log(3);
         if (!nightManager.isStageEnd)
         {
             joyStick.SetActive(false);
