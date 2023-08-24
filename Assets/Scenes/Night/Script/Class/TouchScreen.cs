f6e4c5197644118692a4840cdf19d1a7363da798
CeronMoa <56114106+ceron999@users.noreply.github.com>
2023-08-24 13:17:42 +0900

    230824/정윤석/조이스틱 크기 변경 및 홀로그램 트릭 오류 수정
    1. 조이스틱 크기 변경 코드 추가
    2. 홀로그램 트릭에서 쿨타임이 음수로 나오는 버그 수정

--- Assets/Scenes/Night/Script/Class/TouchScreen.cs
+++ Assets/Scenes/Night/Script/Class/TouchScreen.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -17,13 +18,23 @@ public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
     [SerializeField]
     RectTransform joyStickRecTransform;
 
+    [SerializeField]
+    Slider joyStickSlider;
+    [SerializeField]
+    Transform joyStickTransform;
+
     Vector3 startPos;
     Vector3 moveVector;
+    Vector3 joyStickSizeVector;
 
     [SerializeField]
     float joyStickRange = 10;
     void Start()
     {
+        float startJoyStickSize = GameManager.instance.player.joyStickSize / 100;
+        joyStickSizeVector = new Vector3(startJoyStickSize, startJoyStickSize, startJoyStickSize);
+        joyStickTransform.localScale = joyStickSizeVector;
+
         joyStickRecTransform = handle.GetComponent<RectTransform>();
         moveVector = Vector3.zero;
     }
@@ -72,5 +83,16 @@ public class TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
         }
         else
             joyStickRecTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public void SetJoyStickSize()
+    {
+        joyStickSizeVector = new Vector3(joyStickSlider.value, joyStickSlider.value, joyStickSlider.value);
+        joyStickTransform.localScale = joyStickSizeVector;
+
+        int joyStickSize = (int)(joyStickSlider.value * 100);
+
+        GameManager.instance.player.joyStickSize = joyStickSize;
+        GameManager.instance.SavePlayerData();
     }
 }
